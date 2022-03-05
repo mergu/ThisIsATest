405d2d5c2bd12d862e1b8d87dfa5efd05b5700a1
mergu <mergu@users.noreply.github.com>
2022-03-05 17:37:45 +0000

    Copy repo template

---
+++ template/src/main/java/my/project/denizenscriptpluginmanager/DenizenScriptPluginManager.java
@@ -0,0 +1,17 @@
+package my.project.denizenscriptpluginmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DenizenScriptPluginManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
